b8f750225b0019e99e4b3f08fb74c892adddef3a
liuxishan <1776358671@qq.com>
2021-02-14 00:51:49 +0800

    change

---
+++ src/main/java/com/quartz/study/quartstudy/QuartstudyApplication.java
@@ -0,0 +1,13 @@
+package com.quartz.study.quartstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quartz/study/quartstudy/QuartstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quartz.study.quartstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
