fc3bb8c2110a889081ffcaccb600e4170af33b06
liuxishan <1776358671@qq.com>
2021-06-05 03:30:23 +0800

    upload

---
+++ comstudy/src/main/java/collection/MapTest.java
@@ -0,0 +1,11 @@
+package collection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap<Integer,Integer> map = new ConcurrentHashMap<>();
+        map.put(1,1);
+
+    }
+}
